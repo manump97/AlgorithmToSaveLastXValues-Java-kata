7d7aa1ac791095d4350008e1e4fb4237a36ed509
manump97 <m.montesinos.delpuerto@gmail.com>
2023-06-12 16:39:15 +0200

    Description of the problem

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,27 @@
 package org.example;
 
+/*
+Description:
+Algorithm that needs to be able to save the last X values received.
+
+Format:
+The data input format is the following:
+Orden 1
+...
+Orden N
+
+Instructions:
+print - Return/Print the last value saved
+ctrlZ - delete the last value and be able to recover the last one
+block - Convert the last value in untouchable until unblock instruction
+unblock - Convert the last value in touchable
+
+Message error:
+* Value that is not available
+* Element is blocked
+* Save a value without space
+*/
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
